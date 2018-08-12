7853c37f9739fffc4dacfd973bbca4d201126901
adojem <adojemcovix@gmail.com>
2018-08-12 21:31:30 +0900

    Rendering a home page

--- mern_skelton/server/express.js
+++ mern_skelton/server/express.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
-import template from './../template';
+import Template from './../template';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
 
+// comment out before building for production
+import devBundle from './devBundle';
+
+const CURRENT_WORKING_DIR = process.cwd();
 const app = express();
+
+// comment out before building for production
+devBundle.compile(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,11 +24,15 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
+// mount rutes
 app.use('/', userRoutes);
 app.use('/', authRoutes);
 
 app.get('/', (req, res) => {
-    res.status(200).send(template());
+    res.status(200).send(Template());
 });
 
 // Catch unauthorized errors
