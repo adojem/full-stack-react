0b24ea341a73749abf1bda1e8e8f496425a83eee
adojem <adojemcovix@gmail.com>
2018-10-02 08:51:43 +0900

    Password for auth

--- mern-marketplace/server/models/user.model.js
+++ mern-marketplace/server/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import crypto from 'crypto';
 
 const UserSchema = new mongoose.Schema({
    name: {
@@ -24,3 +25,47 @@ const UserSchema = new mongoose.Schema({
    },
    updated: Date,
 });
+
+UserSchema.virtual('password')
+   .set(function (password) {
+      this._password = password;
+      this.salt = this.makeSalt();
+      this.hashed_password = this.encryptPassword(password);
+   })
+   .get(function () {
+      return this._password;
+   });
+
+UserSchema.path('hashed_password').validate(function (v) {
+   if (this._password && this._password.length < 6) {
+      this.invalidate('password', 'Password must be at least 6 characters.');
+   }
+   if (this.isNew && !this._password) {
+      this.invalidate('password', 'Password is required');
+   }
+}, null);
+
+UserSchema.methods = {
+   authenticate(plainText) {
+      return this.encryptPassword(plainText) === this.hashed_password;
+   },
+   encryptPassword(password) {
+      if (!password) {
+         return '';
+      }
+      try {
+         return crypto
+            .createHmac('sha1', this.salt)
+            .update(password)
+            .digest('hex');
+      }
+      catch (err) {
+         return '';
+      }
+   },
+   makeSalt() {
+      return `${Math.round(new Date().valueOf() * Math.random())}`;
+   },
+};
+
+export default mongoose.model('User', UserSchema);
