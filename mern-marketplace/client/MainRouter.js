12e9da21abb09f98440457b162c176b37b8d77d9
adojem <adojemcovix@gmail.com>
2018-10-06 12:54:00 +0900

    Menu component

--- mern-marketplace/client/MainRouter.js
+++ mern-marketplace/client/MainRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
+import Menu from './core/Menu';
 import Users from './user/Users';
 import Signup from './user/Signup';
 import Signin from './auth/Signin';
@@ -10,6 +11,7 @@ import PrivateRoute from './auth/PrivateRoute';
 
 const MainRouter = () => (
    <div>
+      <Menu />
       <Switch>
          <Route exact path="/" component={Home} />
          <Route path="/users" component={Users} />
---
+++ mern-marketplace/client/core/Menu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Home as HomeIcon } from '@material-ui/icons';
+import auth from '../auth/auth-helper';
+
+const isActive = (history, path) => {
+   if (history.location.pathname === path) {
+      return { color: '#ff4081' };
+   }
+   return { color: '#fff' };
+};
+
+const Menu = withRouter(({ history }) => (
+   <AppBar position="static">
+      <Toolbar>
+         <Typography variant="title" color="inherit">
+            MERN Marketplace
+         </Typography>
+         <Link to="/">
+            <IconButton aria-label="Home" style={isActive(history, '/')}>
+               <HomeIcon />
+            </IconButton>
+         </Link>
+         <Link to="/users">
+            <Button style={isActive(history, '/users')}>Users</Button>
+         </Link>
+         {!auth.isAuthenticated() && (
+            <span>
+               <Link to="/signup">
+                  <Button style={isActive(history, '/signup')}>Sign Up</Button>
+               </Link>
+               <Link to="/signin">
+                  <Button style={isActive(history, '/signin')}>Sign In</Button>
+               </Link>
+            </span>
+         )}
+         {auth.isAuthenticated() && (
+            <span>
+               <Link to={`/user/${auth.isAuthenticated().user._id}`}>
+                  <Button style={isActive(history, '/signup')}>My Profile</Button>
+               </Link>
+               <Button color="inherit" onClick={() => auth.signout(() => history.push('/'))}>
+                  Sign Out
+               </Button>
+            </span>
+         )}
+      </Toolbar>
+   </AppBar>
+));
+
+export default Menu;
