313f2ccbfc713ee02e8caf266a9667ba255fbb12
adojem <adojemcovix@gmail.com>
2018-10-10 17:21:30 +0900

    MyProducts component for shop owners

--- mern-marketplace/client/shop/EditShop.js
+++ mern-marketplace/client/shop/EditShop.js
@@ -8,13 +8,19 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CloudUpload from '@material-ui/icons/CloudUpload';
+import Grid from '@material-ui/core/Grid';
 import Icon from '@material-ui/core/Icon';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import auth from '../auth/auth-helper';
 import { read, update } from './api-shop';
+import MyProducts from '../product/MyProducts';
 
 const styles = theme => ({
+   root: {
+      flexGrow: 1,
+      margin: 30,
+   },
    card: {
       textAlign: 'center',
       paddingBottom: theme.spacing.unit * 2,
@@ -127,70 +133,83 @@ class EditShop extends Component {
       }
 
       return (
-         <Card className={classes.card}>
-            <CardContent>
-               <Typography variant="title" component="h2" className={classes.title}>
-                  Edit Shop
-               </Typography>
-               <br />
-               <Avatar src={logoUrl} className={classes.bigAvatar} />
-               <br />
-               <label htmlFor="icon-button-file">
-                  <input
-                     accept="image/*"
-                     className={classes.input}
-                     type="file"
-                     id="icon-button-file"
-                     onChange={this.handleChange('image')}
-                  />
-                  <Button variant="raised" color="default" component="span">
-                     Change Logo
-                     <CloudUpload className={classes.rightIcon} />
-                  </Button>
-               </label>
-               <br />
-               <TextField
-                  id="name"
-                  label="Name"
-                  value={name}
-                  className={classes.textField}
-                  margin="normal"
-                  onChange={this.handleChange('name')}
-               />
-               <br />
-               <TextField
-                  id="multiline-flexible"
-                  label="Description"
-                  multiline
-                  rows="3"
-                  value={description}
-                  className={classes.textField}
-                  margin="normal"
-                  onChange={this.handleChange('description')}
-               />
-               <br />
-               <Typography variant="subheading" component="h4" className={classes.subheading}>
-                  {`Owner: ${owner}`}
-               </Typography>
-               <br />
-               {error && (
-                  <Typography component="p" color="error">
-                     <Icon className={classes.error}>error</Icon>
-                     {error}
-                  </Typography>
-               )}
-            </CardContent>
-            <CardActions>
-               <Button
-                  color="primary"
-                  variant="raised"
-                  className={classes.submit}
-                  onClick={this.clickSubmit}
-               >
-                  Update
-               </Button>
-            </CardActions>
-         </Card>
+         <div className={classes.root}>
+            <Grid container spacing={24}>
+               <Grid item xs={6} sm={6}>
+                  <Card className={classes.card}>
+                     <CardContent>
+                        <Typography variant="headline" component="h2" className={classes.title}>
+                           Edit Shop
+                        </Typography>
+                        <br />
+                        <Avatar src={logoUrl} className={classes.bigAvatar} />
+                        <br />
+                        <label htmlFor="icon-button-file">
+                           <input
+                              accept="image/*"
+                              className={classes.input}
+                              type="file"
+                              id="icon-button-file"
+                              onChange={this.handleChange('image')}
+                           />
+                           <Button variant="raised" color="default" component="span">
+                              Change Logo
+                              <CloudUpload className={classes.rightIcon} />
+                           </Button>
+                        </label>
+                        <br />
+                        <TextField
+                           id="name"
+                           label="Name"
+                           value={name}
+                           className={classes.textField}
+                           margin="normal"
+                           onChange={this.handleChange('name')}
+                        />
+                        <br />
+                        <TextField
+                           id="multiline-flexible"
+                           label="Description"
+                           multiline
+                           rows="3"
+                           value={description}
+                           className={classes.textField}
+                           margin="normal"
+                           onChange={this.handleChange('description')}
+                        />
+                        <br />
+                        <Typography
+                           variant="subheading"
+                           component="h4"
+                           className={classes.subheading}
+                        >
+                           {`Owner: ${owner}`}
+                        </Typography>
+                        <br />
+                        {error && (
+                           <Typography component="p" color="error">
+                              <Icon className={classes.error}>error</Icon>
+                              {error}
+                           </Typography>
+                        )}
+                     </CardContent>
+                     <CardActions>
+                        <Button
+                           color="primary"
+                           variant="raised"
+                           className={classes.submit}
+                           onClick={this.clickSubmit}
+                        >
+                           Update
+                        </Button>
+                     </CardActions>
+                  </Card>
+               </Grid>
+               <Grid item xs={6} sm={6}>
+                  <MyProducts shopId={this.match.params.shopId} />
+               </Grid>
+            </Grid>
+         </div>
       );
    }
 }
