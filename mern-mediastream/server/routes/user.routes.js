8b35f23ec27e4a6d16f8d8998b347bf4ea4bcfc2
adojem <adojemcovix@gmail.com>
2018-10-19 14:59:52 +0900

    Loading a user by ID to read, update, or delete

--- mern-mediastream/server/routes/user.routes.js
+++ mern-mediastream/server/routes/user.routes.js
@@ -8,6 +8,12 @@ router
    .get(userCtrl.list)
    .post(userCtrl.create);
 
-router.route('/api/users/:userId');
+router
+   .route('/api/users/:userId')
+   .get(userCtrl.read)
+   .put(userCtrl.update)
+   .delete(userCtrl.remove);
+
+router.param('userId', userCtrl.userByID);
 
 export default router;
