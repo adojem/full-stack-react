f2bd4e22ec67710ca3e866b950cf4dddbd9688e7
adojem <adojemcovix@gmail.com>
2018-10-19 10:27:14 +0900

    Frontend views with React

---
+++ mern-mediastream/template.js
@@ -0,0 +1,13 @@
+export default () => `
+   <!DOCTYPE html>
+   <html lang="ja">
+   <head>
+      <meta charset="UTF-8"/>
+      <title>MERN Mediastrem</title>
+   </head>
+   <body>
+      <div id="root"></div>
+      <script src="/dist/bundle.js"></script>
+   </body>
+   </html>
+`;
