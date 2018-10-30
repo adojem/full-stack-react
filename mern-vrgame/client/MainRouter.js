186af547d69cd2fbb3bb2e7bb0afef4ce582c48a
adojem <adojemcovix@gmail.com>
2018-10-30 12:16:02 +0900

    Root React component

---
+++ mern-vrgame/client/MainRouter.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const MainRouter = () => (
+   <Fragment>
+      <Switch>
+         <Route />
+      </Switch>
+   </Fragment>
+);
+
+export default MainRouter;
